99293f94e1a737ec53c0e46d2d763cfdf4489dc0
Alex <antonio.alex@hotmail.com>
2022-03-21 22:04:25 -0300

    superreges

--- SuperReges/src/br/com/superreges/modelo/Fornecedor.java
+++ SuperReges/src/br/com/superreges/modelo/Fornecedor.java
@@ -10,11 +10,61 @@ package br.com.superreges.modelo;
  */
 public class Fornecedor {
     
+    
     private String nome;
     private String cnpj;
     private String representacao; //marcas que o fornecedor representa
     private String email;
-    private String telefone;
-    
+    private String telefone;    
     private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRepresentacao() {
+        return representacao;
+    }
+
+    public void setRepresentacao(String representacao) {
+        this.representacao = representacao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+  
 }
--- SuperReges/src/br/com/superreges/modelo/ItemVenda.java
+++ SuperReges/src/br/com/superreges/modelo/ItemVenda.java
@@ -10,4 +10,32 @@ package br.com.superreges.modelo;
  */
 public class ItemVenda {
     
+    private Produto produto;    
+    private double quantidade;
+    private double valorTotal;
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValorTotal() {
+        return this.produto.getPreco() * quantidade;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    
 }
