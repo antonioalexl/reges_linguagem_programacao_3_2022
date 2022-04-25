e875db7892e56682536f0e54245c2e56ef6715a2
Administrador <antonio.alex@hotmail.com>
2022-04-25 17:56:41 -0300

    aula-11

---
+++ Aula08-Avaliacao/src/aula08/avaliacao/Aula08Avaliacao.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula08.avaliacao;
+
+/**
+ *
+ * @author alex.lopes
+ */
+public class Aula08Avaliacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Exercicios exe = new Exercicios();
+        exe.imprimirNumerosImpares();
+    }
+    
+    
+    
+}
---
+++ Aula08-Avaliacao/src/aula08/avaliacao/Exercicios.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula08.avaliacao;
+
+/**
+ *
+ * @author alex.lopes
+ */
+public class Exercicios {
+
+    public void imprimirNumerosImpares() {
+
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
